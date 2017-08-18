2565dd640cb21a2cc845d06dc541895365503b81
zhengzexin-python <zzz.kagari@gmail.com>
2017-08-19 00:01:36 +0800

    路由练习小项目

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ProductComponent} from './product/product.component';
 
 const routes: Routes = [
   {
-    path: '',
-    children: []
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'product', component: ProductComponent
   }
 ];
 
