c99696e685e7a43aea96ef4b02ff282be55eaef9
zhengzexin-python <zzz.kagari@gmail.com>
2017-08-21 23:46:50 +0800

    路由配置及导航

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {ProductComponent} from './product/product.component';
+import {Code404Component} from "./code404/code404.component";
 
 const routes: Routes = [
   {
@@ -9,11 +10,15 @@ const routes: Routes = [
   },
   {
     path: 'product', component: ProductComponent
-  }
+  },
+  {
+    path: '**', component: Code404Component
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
